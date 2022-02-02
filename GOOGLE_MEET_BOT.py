02ce03ba66c7119060ce9dc8a8128bb840f50017
iamgeekyaseem <92142983+iamgeekyaseem@users.noreply.github.com>
2022-02-03 02:30:00 +0530

    Update GOOGLE_MEET_BOT.py

--- GOOGLE_MEET_BOT.py
+++ GOOGLE_MEET_BOT.py
@@ -1,20 +1,20 @@
 # works in both windows and Mac
 from time import sleep
-import pyautogui as chirag
+import pyautogui as aseem
 import schedule
 import webbrowser
 import subprocess
 # the link of the class
-link = "meet.google.com/yrf-feui-ykz"
+link = "meet.google.com/[meet-code]"
 # the timing of the class
 time = "19:27"
 subprocess.Popen("C:\Program Files\Google\Chrome\Application\chrome.exe")
 def join():
     webbrowser.open_new_tab('https://'+ link)
     sleep(10)
-    chirag.hotkey('Ctrl','d') #FIXED
-    chirag.hotkey('Ctrl','e') #FIXED
-    chirag.click(1413,576)
+    aseem.hotkey('Ctrl','d') #FIXED
+    aseem.hotkey('Ctrl','e') #FIXED
+    aseem.click(1413,576)
 schedule.every().day.at(time).do(join)
 while True:
     schedule.run_pending()
